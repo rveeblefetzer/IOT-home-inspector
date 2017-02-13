c7accbf980a8cf639da5ec1ddfa40a1df1fafaa5
JoeyDeRosa <joeyderosa11.jd@gmail.com>
2017-02-13 15:27:03 -0800

    switched to bing for search resaults

--- IOThomeinspector/IOThomeinspector/google_api.py
+++ IOThomeinspector/IOThomeinspector/google_api.py
@@ -1,12 +1,18 @@
 """User search using google."""
 
 
-import webbrowser
+from bs4 import BeautifulSoup
+import requests
 
 
 def make_search(key_words):
-  """Using the key words make a search request for new firmware."""
-  base_url = 'https://google.com/#q='
-  search_term = '+'.join(key_words.split(' ')) + '+firmware+update'
-  search = base_url + search_term
-  webbrowser.open(search, new=0, autoraise=True)
+    """Using the key words make a search request for new firmware."""
+    soups = []
+    extensions = ['+firmware+update', '+security+vulnerabilities']
+    for search_extension in extensions:
+        print('https://bing.com/search?q=' + '+'.join(key_words.split(' ')) + search_extension)
+        search_term = '+'.join(key_words.split(' ')) + search_extension
+        search_request = requests.get('https://bing.com/search?q=' + search_term, auth=('user', 'pass'))
+        soup = BeautifulSoup(search_request.text, 'html.parser')
+        soups.append(soup)
+    return soups
