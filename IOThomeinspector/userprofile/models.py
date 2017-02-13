4fa614676aad9833149ded0cdb75e3e45e401c15
regenalgrant <regenal@mac.com>
2017-02-13 09:54:26 -0800

    requirements added and models started.

---
+++ IOThomeinspector/userprofile/models.py
@@ -0,0 +1,7 @@
+"""Creating Models for user profile."""
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
