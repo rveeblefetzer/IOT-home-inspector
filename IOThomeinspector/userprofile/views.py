b24f22d530726c934e6668dea9228c1e08df0a0b
copenbacon <sclary50@gmail.com>
2017-02-14 14:51:59 -0800

    profile view, edit profile view

--- IOThomeinspector/userprofile/views.py
+++ IOThomeinspector/userprofile/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+"""Views for the User Profile."""
 
-# Create your views here.
+from userprofile.forms import EditProfileForm
+from django.conf import settings
+from .models import UserProfile
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, UpdateView
+from django.shortcuts import redirect
+
+
+class ProfileView(TemplateView):
+    """Return the Profile View inheriting from TemplateView."""
+
+    template_name = 'userprofile/profile.html'
+
+    def get_context_data(self, username=None):
+        """Get profiles and return them."""
+        if self.request.user.is_authenticated():
+            profile = self.request.user.profile
+            return {'profile': profile}
+        else:
+            error_message = "You're not signed in."
+            return {'error': error_message}
+
+
+class EditProfileView(LoginRequiredMixin, UpdateView):
+    """Edit the authenticated users profile."""
+
+    template_name = 'userprofile/edit_profile.html'
+    model = UserProfile
+    form_class = EditProfileForm
+
+    def get_object(self):
+        """Get the user profile object."""
+        return self.request.user.profile
+
+    def form_valid(self, form):
+        """Save model forms to database."""
+        self.object = form.save()
+        self.object.user.first_name = form.cleaned_data['First Name']
+        self.object.user.last_name = form.cleaned_data['Last Name']
+        self.object.user.email = form.cleaned_data['Email']
+        self.object.user.save()
+        self.object.save()
+        return redirect('profile')
