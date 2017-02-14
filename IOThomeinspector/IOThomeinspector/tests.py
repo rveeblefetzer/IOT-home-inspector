e6a3240fe3f5da8257812af8236ab50c222d2d4e
JoeyDeRosa <joeyderosa11.jd@gmail.com>
2017-02-13 16:32:42 -0800

    fixed tests

--- IOThomeinspector/IOThomeinspector/tests.py
+++ IOThomeinspector/IOThomeinspector/tests.py
@@ -14,17 +14,17 @@ class SearchResaultTestCase(TestCase):
 
     def test_soups_return(self):
         """Test that the soups list being returned is getting appended to."""
-        self.assertTrue(len(self.soups) is 2)
+        self.assertTrue(len(self.soups) == 2)
 
     def test_type_of_items_in_soups_is_beautiful_soup(self):
         """Test that the items in the soups list getting returned are beautiful soup objects."""
         for soup in self.soups:
-            self.assertTrue(type(soup) is bs4.BeautifulSoup)
+            self.assertTrue(type(soup) == bs4.BeautifulSoup)
 
     def test_getting_the_right_html_firmware(self):
         """Test that the html in the beautiful soup object contains the search resaults for firmware updates."""
-        self.assertTrue(self.soups[0].title is '<title>philips hue firmware update - Bing</title>')
+        self.assertTrue(self.soups[0].title == '<title>philips hue firmware update - Bing</title>')
 
     def test_getting_the_right_html_security(self):
         """Test that the html in the beautiful soup object contains the search resaults for security vulnerabilities."""
-        self.assertTrue(self.soups[1].title is '<title>philips hue security vulnerabilities - Bing</title>')
+        self.assertTrue(self.soups[1].title == '<title>philips hue security vulnerabilities - Bing</title>')
