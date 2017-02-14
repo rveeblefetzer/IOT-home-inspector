0855111a844130ad7f962f81e6226be178e1234b
copenbacon <sclary50@gmail.com>
2017-02-14 11:51:54 -0800

    browser version

--- IOThomeinspector/IOThomeinspector/views.py
+++ IOThomeinspector/IOThomeinspector/views.py
@@ -11,5 +11,28 @@ class HomePageView(TemplateView):
 
     def get_context_data(self):
         """View for the home page."""
-        context = {}
+        ua_profile = self.request.META['HTTP_USER_AGENT'].split()
+        check = True
+        for item in ua_profile:
+            if "Chrome" in item:
+                browser = item
+                if float(browser.split('/')[1][0:4]) < 56:
+                    check = False
+                break
+            elif "Firefox" in item:
+                browser = item
+                if float(browser.split('/')[1][0:4]) < 51:
+                    check = False
+                break
+            elif "Safari" in item:
+                browser = item
+                if float(browser.split('/')[1][0:4]) < 10:
+                    check = False
+            else:
+                browser = None
+        if check is True:
+            browser = "You are using " + browser + ", you are up to date!"
+        else:
+            browser = "You are using " + browser + ", you are not up to date!"
+        context = {'browser': browser}
         return context
