6b053f43192a9fc66fdc1fc19da8e2aa3ff6e2df
JoeyDeRosa <joeyderosa11.jd@gmail.com>
2017-02-16 10:36:06 -0800

    search every possible ip on local network

--- IOThomeinspector/IOThomeinspector/devices_n_ports.py
+++ IOThomeinspector/IOThomeinspector/devices_n_ports.py
@@ -3,22 +3,30 @@ import nmap
 import socket
 from pprint import pprint
 nm = nmap.PortScanner()
+ip_addresses = []
+scans = []
 this_local_ip = socket.gethostbyname(socket.gethostname())
-this_local_ip = this_local_ip.split('.')
-this_local_ip[2] = '0'
-this_local_ip[3] = '0'
-this_local_ip = ".".join(this_local_ip)
-this_local_ip += '/24'
-the_scan = nm.scan(this_local_ip, '23, 9100')
-pprint(the_scan)
+test_ip = this_local_ip.split('.')
+for index_one in range(16,32):
+    test_ip[1] = str(index_one)
+    for index_two in range(256):
+        test_ip[2] = str(index_two)
+        test_ip[3] = '0'
+        ip_addresses.append('.'.join(test_ip))
+for ip in ip_addresses:
+    scans.append(nm.scan(ip + '/24', '23, 9100', '--stats-every 2s'))
 
-print(the_scan['scan'][this_local_ip]['tcp'][port]['version'])
-print(the_scan['scan'][this_local_ip]['tcp'][port]['product'])
-print(the_scan['scan'][this_local_ip]['tcp'][port]['state'])
+# print(the_scan['scan'][this_local_ip]['tcp'][port]['version'])
+# print(the_scan['scan'][this_local_ip]['tcp'][port]['product'])
+# print(the_scan['scan'][this_local_ip]['tcp'][port]['state'])
 
-for i in the_scan['scan']:
-    for x in the_scan['scan'][i]['tcp']:
-        pprint(the_scan['scan'][i]['tcp'][x]['version'])
-        pprint(the_scan['scan'][i]['tcp'][x]['product'])
-        pprint(the_scan['scan'][i]['tcp'][x]['state'])
+for scan in scans:    
+    for i in the_scan['scan']:
+        for x in the_scan['scan'][i]['tcp']:
+            if the_scan['scan'][i]['tcp'][x]['state'] == 'open':
+                pprint(scan)
+                pprint(the_scan['scan'][i]['tcp'][x]['version'])
+                pprint(the_scan['scan'][i]['tcp'][x]['product'])
+                pprint(the_scan['scan'][i]['tcp'][x]['state'])
+                print('------------------')
 
